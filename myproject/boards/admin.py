34c68657dc127d7905afae0fc8339d00148689aa
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-07-31 01:14:41 +0530

    🌷🌺🌼 Views and Templates added
    shh tests dont work

--- myproject/boards/admin.py
+++ myproject/boards/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+# class TopicInline(admin.TabularInline):
+#     model = Topic
+
+class BoardAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    # inlines = [TopicInline]
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Topic)
+admin.site.register(Post)
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Board
+
 # Create your views here.
 def home(request):
-    return HttpResponse("this is the home page of BOARDS")
+    boards_list = Board.objects.all()
+    context = {'boards_list': boards_list}
+    return render(request, 'boards/index.html', context)
+    # return HttpResponse("this is the home page of BOARDS")
